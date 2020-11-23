895cfce483237132b704312abd6a378b4d3da71e
Zvonimir <zvonimirplivelic91@gmail.com>
2020-11-23 09:57:34 +0100

    created landscape layout, made some design and functionality changes, code cleanup

--- app/src/main/java/com/zvonimirplivelic/tiptime/MainActivity.kt
+++ app/src/main/java/com/zvonimirplivelic/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.zvonimirplivelic.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import com.zvonimirplivelic.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -14,10 +18,11 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->  handleKeyEvent(view, keyCode)}
     }
 
     private fun calculateTip() {
-        val stringInput = binding.costOfService.text.toString()
+        val stringInput = binding.costOfServiceEditText.text.toString()
         val cost = stringInput.toDoubleOrNull()
         if(cost == null || cost == 0.0) {
             displayTip(0.0)
@@ -42,5 +47,14 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if(keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
